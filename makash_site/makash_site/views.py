6d5fb339b0269bec2e59f56e1c63f90d45864377
Mukto Akash <makash@conestogac.on.ca>
2023-09-26 15:51:37 -0400

    Set up requirements.txt and fixed some pylint issues

--- makash_site/makash_site/views.py
+++ makash_site/makash_site/views.py
@@ -1,7 +1,10 @@
 # mysite/views.py
-
+""" A simple view to demostrate django.
+"""
 from django.http import HttpResponse
 
 
 def index(request):
+    """ Returns the text for the webpage
+    """
     return HttpResponse('CG-OLRN1508-Assignment-1 <br> Mukto Akash')
--- makash_site/tests/test_views.py
+++ makash_site/tests/test_views.py
@@ -1,8 +1,11 @@
 # test_views.py
+""" Test module for the view
+"""
 
 def test_index_ok(client):
-    # Make a GET request to / and store the response object
-    # using the Django test client.
+    """ Make a GET request to / and store the response object
+    using the Django test client.
+    """
     response = client.get('/')
     # Assert that the status_code is 200 (OK)
     assert response.status_code == 200
